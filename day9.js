8ad80092d7bdd8d1cb4c6db64a2021e7947ef4e6
Farrukh <farrukh.agh.edu@gmail.com>
2021-12-09 14:59:34 +0100

    newcard function has been added

--- day9.js
+++ day9.js
@@ -1,39 +1,36 @@
-               
-            var result = [];
+var usedNums = new Array(76);
 
-            function chart(){
-            var button='';
-            for(var i=1; i<91; i++){
-                button += "<button id='" + i +"'>" + i + "</button>"; // <button id=1...>
-            }
-            return button;
-            }
-            setInterval(
-            function genRandomNumber(){
+function newCard() {
+	//Starting loop through each square card
+	for(var i=0; i < 24; i++) {  //<--always this code for loops. change in red
+		setSquare(i);
+	}
+}
 
-            var num = Math.floor(Math.random() * 90 + 1);
-            for(var i = 0; i < result.length; i++){
-                if(num == result[i]){
-                num = Math.floor(Math.random() * 90 + 1);
-                i = 0;
-                }
-            }
-            document.getElementById("number").innerHTML = num;
-            document.getElementById(num).style.backgroundColor = "green";
-            document.getElementById(num).style.color = "white";
-            
-            // This line of code does not work because it can coloured only one cell which is selected using id.
-            // I want to change all cell background color which has generated number.
-            const x = document.getElementById("td1");
-            if(num == x.innerHTML.valueOf()){
-                x.style.backgroundColor = "green"
-            }
-            
-           
-            result.push(num);
-            // const output = document.getElementById("output1").innerHTML = result;
-    
-            }, 1000);
+function setSquare(thisSquare) {
+	var currSquare = "square"+thisSquare;
+	var newNum;
+	
+	var colPlace =new Array(0,1,2,3,4,0,1,2,3,4,0,1,3,4,0,1,2,3,4,0,1,2,3,4);
+	
+	do {
+		newNum =(colPlace[thisSquare] * 15) + getNewNum() + 1;
+	}
+	while (usedNums[newNum]);
+	
+	usedNums[newNum] = true;
+	document.getElementById(currSquare).innerHTML = newNum;
+}
 
-            
-            document.getElementById("buttons").innerHTML = chart();
+function getNewNum() {
+	return Math.floor(Math.random() * 75);
+	
+}
+
+function anotherCard() {
+	for(var i=1; i<usedNums.length; i++) {
+	usedNums[i] = false;
+	}
+	
+	newCard();
+}

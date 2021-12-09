8e72009eb441b386ab059bf36e54d289675f7ee0
Farrukh <farrukh.agh.edu@gmail.com>
2021-12-09 14:50:13 +0100

    in js file some functions deleted and i added new

--- day9.js
+++ day9.js
@@ -1,68 +1,39 @@
-let numbers = new Set()
-.add("1")
-.add("2")
-.add("3")
-.add("4")
-.add("5")
-.add("6")
-.add("7")
-.add("8")
-.add("9")
-.add("10")
-.add("11")
-.add("12")
-.add("13")
-.add("10")
-.add("14")
-.add("15")
-.add("16")
-.add("17")
-.add("18")
-.add("19")
-.add("20")
-.add("21")
-.add("22")
-.add("23")
-.add("24")
-.add("25")
-.add("26")
-.add("27")
-.add("28")
-.add("29")
-.add("30")
+               
+            var result = [];
 
-let called = Array.from(numbers);
+            function chart(){
+            var button='';
+            for(var i=1; i<91; i++){
+                button += "<button id='" + i +"'>" + i + "</button>"; // <button id=1...>
+            }
+            return button;
+            }
+            setInterval(
+            function genRandomNumber(){
 
-let display = new Array();
+            var num = Math.floor(Math.random() * 90 + 1);
+            for(var i = 0; i < result.length; i++){
+                if(num == result[i]){
+                num = Math.floor(Math.random() * 90 + 1);
+                i = 0;
+                }
+            }
+            document.getElementById("number").innerHTML = num;
+            document.getElementById(num).style.backgroundColor = "green";
+            document.getElementById(num).style.color = "white";
+            
+            // This line of code does not work because it can coloured only one cell which is selected using id.
+            // I want to change all cell background color which has generated number.
+            const x = document.getElementById("td1");
+            if(num == x.innerHTML.valueOf()){
+                x.style.backgroundColor = "green"
+            }
+            
+           
+            result.push(num);
+            // const output = document.getElementById("output1").innerHTML = result;
+    
+            }, 1000);
 
-
-function getRandomNum()
-{
-function rando()
-{
-for (let i = called.length - 1; i > 0; i++) 
-{
-    const j = Math.floor(Math.random() * called.length);
-    const number = called[i];
-    called[i] = called[j];
-    called[j] = number;
-    return number;
-
-    //let show = called[Math.floor(Math.random() * called.length)];
-    //return show;
-}
-//document.getElementById('bingo').innerHTML = display[0];
-}
-let index = rando();
-document.getElementById('bingo').innerHTML = index;
-display.push(index);
-
-
-}
-function show()
-{
-for(let n = 0; n < display.length; n++)
-{
-document.getElementById('reveal').innerHTML += "<br/>" + display[n] + "<br/>";
-}
-} 
+            
+            document.getElementById("buttons").innerHTML = chart();
